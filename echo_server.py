e7b9f72f624d45451d67ebb5d1baf5fc31dd6346
mukund26 <m.agarwalhp@gmail.com>
2017-08-30 01:34:51 +0530

    Echo server client prog

---
+++ echo_server.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+#echo_server.py
+import socket
+import sys
+host="127.0.0.1"
+port=12345
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(1)
+
+while True:
+	conn,addr=s.accept()
+	print('Connected by :',addr)
+	data=conn.recv(1024)
+	if not data : break
+	print sys.getsizeof(data)
+	conn.sendall(data)
+conn.close() 
+
+
