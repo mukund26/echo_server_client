243d9a2f5af78b9af930f952a47afab0fab594de
cominixo01 <lucasdutraluza@gmail.com>
2017-10-14 11:38:45 -0300

    Python 3 support
    This adds python 3 support

--- echo_client.py
+++ echo_client.py
@@ -8,37 +8,37 @@ def pad(s, n):
 	for i in range(nsp):
 		rs = rs + " "
 	rs = rs + ":: ]"
-	return rs;
+	return rs
 
 def center(s, n):
 	nsp = n - len(s) - 12
-	rs = "[ ... ";
-	for i in range(nsp/2):
+	rs = "[ ... "
+	for i in range(int(nsp/2)):
 		rs = rs + " "
-	rs = rs + s;
-	for i in range(nsp/2):
+	rs = rs + s
+	for i in range(int(nsp/2)):
 		rs = rs + " "
 
-	if (nsp % 2 != 0):
+	if nsp % 2 != 0:
 		rs = rs + " "
 
 	rs = rs + "... ]"
-	return rs;
+	return rs
 
 host = "127.0.0.1"
 port = 12345
-data = "Hello Python!"
+data = b"Hello Python!"
 
-print center("connecting to host " + host + ", port " + str(port), 70)
+print(center("connecting to host " + host + ", port " + str(port), 70))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-print pad("sending data to host: " + data, 70)
+print(pad("sending data to host: " + data.decode("utf-8"), 70))
 s.sendall(data)
-print pad("receiving data from host " + host + ", port " + str(port), 70)
+print(pad("receiving data from host " + host + ", port " + str(port), 70))
 data = s.recv(1024)
 
-print pad("received data: " +str(data), 70)
+print(pad("received data: " + data.decode("utf-8"), 70))
 
 s.close()
